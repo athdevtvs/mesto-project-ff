06b01faf4c7fb5f517b87ee2f188a8536dd793ef
Tatiana Vaganova <athdev@yandex.ru>
2024-01-21 14:12:44 +0300

    fix/fixes-based-on-comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
+// Кроме того что требовалось я еще:
+//   1. убрала объект cardData с переопределением имен ключей,
+//      т.к. он избыточный. Ключи уже придуманы в card.js и лучше,
+//      наверное, ими и пользоваться.
+//   2. убрала функцию addCard т.к. там только одно действие и я его
+//      больше нигде не переиспользую.
+//   3. из deleteCard убрала строчку с closest т.к. она уже не нужна.
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 
@@ -6,42 +14,28 @@ const container = document.querySelector(".content");
 const placesContainer = container.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function createCard(cardData) {
+function createCard(card, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
 
-  cardImage.src = cardData.src;
-  cardImage.alt = cardData.alt;
-  cardElement.querySelector(".card__title").textContent = cardData.title;
+  cardImage.src = card.link;
+  cardImage.alt = card.description;
+  cardElement.querySelector(".card__title").textContent = card.name;
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => deleteCard(cardElement));
 
   return cardElement;
 }
 
-function addCard(cardElement, deleteCard) {
-  placesContainer.append(cardElement);
-
-  const cardDeleteButton = container.querySelector(
-    ".card:last-child .card__delete-button"
-  );
-  cardDeleteButton.addEventListener("click", () =>
-    deleteCard(cardDeleteButton)
-  );
-}
-
 // @todo: Функция удаления карточки
-const deleteCard = (cardDeleteButton) => {
-  const cardItem = cardDeleteButton.closest(".card");
+const deleteCard = (cardItem) => {
   cardItem.remove();
 };
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((card) => {
-  const cardData = {
-    src: card.link,
-    title: card.name,
-    alt: card.description,
-  };
-
-  const cardElement = createCard(cardData);
-  addCard(cardElement, deleteCard);
+  const cardElement = createCard(card, deleteCard);
+  placesContainer.append(cardElement);
 });
